5166be5a30a65da6b72124409d12ad4d8702fb26
wq <941058084@qq.com>
2021-02-15 18:52:12 +0800

    架构探险第一章节demo练习

---
+++ src/main/java/org/smart4j/chapter1/HelloServlet.java
@@ -0,0 +1,28 @@
+package org.smart4j.chapter1;
+
+import javafx.scene.input.DataFormat;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * @author xxx
+ * @date 2021/2/15 18:06
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        String currentTime = dateFormat.format(new Date());
+        req.setAttribute("currentTime", currentTime);
+        req.getRequestDispatcher("/WEB-INF/jsp/hello.jsp").forward(req, resp);
+    }
+}
---
+++ src/main/webapp/WEB-INF/jsp/hello.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: 94105
+  Date: 2021/2/15
+  Time: 18:17
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Hello</title>
+</head>
+<body>
+<h1>Hello!</h1>
+<h2>当前时间：${currentTime}</h2>
+</body>
+</html>
